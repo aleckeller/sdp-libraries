6d3efb7cc608a6a42e3ce045d407903076d0b7ff
aleckeller <alec.keller.17@gmail.com>
2020-01-02 16:45:48 -0500

    Update github_enterprise_constructor.groovy

--- bitbucket/github_enterprise_constructor.groovy
+++ bitbucket/github_enterprise_constructor.groovy
@@ -6,8 +6,8 @@
 @Init
 void call(Map context) {
   node{
-      sh "chmod -R u+w .git"
-      unstash "workspace"
+      //sh "chmod -R u+w .git"
+      //unstash "workspace"
 
       env.GIT_URL = scm.getUserRemoteConfigs()[0].getUrl()
       env.GIT_CREDENTIAL_ID = scm.getUserRemoteConfigs()[0].credentialsId.toString()
