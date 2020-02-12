1c6e080a8ff8cf0fcda0a4b5eb3900bd1dcf3ff1
aleckeller <alec.keller.17@gmail.com>
2020-02-12 15:22:33 -0500

    Update github_enterprise_constructor.groovy

--- bitbucket/github_enterprise_constructor.groovy
+++ bitbucket/github_enterprise_constructor.groovy
@@ -9,32 +9,32 @@ void call(Map context) {
      try{
         sh "chmod -R u+w .git"
         unstash "workspace"
-     }
-    catch(Exception e){
-      println "Skipping unstashing workspace.."
-    }
 
-      env.GIT_URL = scm.getUserRemoteConfigs()[0].getUrl()
-      env.GIT_CREDENTIAL_ID = scm.getUserRemoteConfigs()[0].credentialsId.toString()
-      def parts = env.GIT_URL.split("/")
-      for (part in parts){
-          parts = parts.drop(1)
-          if (part.contains(".")) break
+        env.GIT_URL = scm.getUserRemoteConfigs()[0].getUrl()
+        env.GIT_CREDENTIAL_ID = scm.getUserRemoteConfigs()[0].credentialsId.toString()
+        def parts = env.GIT_URL.split("/")
+        for (part in parts){
+            parts = parts.drop(1)
+            if (part.contains(".")) break
+        }
+        env.ORG_NAME = parts.getAt(0)
+        env.REPO_NAME = parts[1..-1].join("/") - ".git"
+        env.GIT_SHA = sh(script: "git rev-parse HEAD", returnStdout: true).trim()
+
+        if (env.CHANGE_TARGET){
+          env.GIT_BUILD_CAUSE = "pr"
+        } else {
+          env.GIT_BUILD_CAUSE = sh (
+            script: 'git rev-list HEAD --parents -1 | wc -w', // will have 2 shas if commit, 3 or more if merge
+            returnStdout: true
+          ).trim().toInteger() > 2 ? "merge" : "commit"
+        }
+
+        println "Found Git Build Cause: ${env.GIT_BUILD_CAUSE}"
       }
-      env.ORG_NAME = parts.getAt(0)
-      env.REPO_NAME = parts[1..-1].join("/") - ".git"
-      env.GIT_SHA = sh(script: "git rev-parse HEAD", returnStdout: true).trim()
-
-      if (env.CHANGE_TARGET){
-        env.GIT_BUILD_CAUSE = "pr"
-      } else {
-        env.GIT_BUILD_CAUSE = sh (
-          script: 'git rev-list HEAD --parents -1 | wc -w', // will have 2 shas if commit, 3 or more if merge
-          returnStdout: true
-        ).trim().toInteger() > 2 ? "merge" : "commit"
+      catch(Exception e){
+        println "Skipping unstashing workspace.."
       }
-
-      println "Found Git Build Cause: ${env.GIT_BUILD_CAUSE}"
   }
   return
 }
